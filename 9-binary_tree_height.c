55cde9c7769274440e6c1ac30ade445fd69903c8
Kwesi20000 <hendrickskumi@yahoo.com>
2024-01-04 00:05:15 -0800

    hendy

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_left, height_right;
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-	if (tree == NULL)
-		return (0);
-
-	height_left = binary_tree_height(tree->left);
-	height_right = binary_tree_height(tree->right);
-
-	return (1 + (height_left > height_right ? height_left : height_right));
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
