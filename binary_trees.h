eaae0ae93416bc37a25674d1dd84129093451fb5
Kwesi20000 <hendrickskumi@yahoo.com>
2024-01-04 14:57:13 -0800

    hendy

--- binary_trees.h
+++ binary_trees.h
@@ -64,5 +64,7 @@ heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
 
 #endif /* _BINARY_TREES_H_ */
